f218d62210fed6c93bb2764e9157e743fd9a8d15
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-12-28 10:01:08 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Guided_Assignment_How_To
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
