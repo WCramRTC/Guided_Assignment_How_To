7b8fce18e36c7da448f83b255ca5990e09be0d28
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-12-28 10:17:09 -0800

    Initial Upload

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-        }
-    }
-}
+        } // Main
+
+    } // class
+
+} // namespace
+
+// Requirements
+// Visual Studio 2022
+// GitHub Account ( www.GitHub.com )
+
+// Requirements
+// Create a new project
+// Follow the steps to fulfill the requirements
+// Get it pushed online, with a public repository
+// Submit GitHub repository URL in the TextBox
